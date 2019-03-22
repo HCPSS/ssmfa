77d50537aac6250db61408e5ae93dcc689e9079f
Nick Vissari <nvissari@hcpss.org>
2019-03-22 18:11:16 -0400

    Allow any domain for test mail server

--- mail-dev/docker-entrypoint.sh
+++ mail-dev/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# replace example domain with domain from env
+cat /etc/postfix/main.cf | sed "s/example.dev/$MAIL_DOMAIN/g" > /tmp/main.cf
+mv /tmp/main.cf /etc/postfix/main.cf
+cat /etc/postfix/virtual | sed "s/example.dev/$MAIL_DOMAIN/g" > /tmp/virtual
+mv /tmp/virtual /etc/postfix/virtual
+
 service postfix start
 
 postmap /etc/postfix/virtual
